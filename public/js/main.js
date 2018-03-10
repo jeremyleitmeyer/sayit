cc5464fcff567f51994d9da2976d78d662b23dc7
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2018-03-10 17:17:10 -0500

    finished banner, button and add ?

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
-// submit on enter
+var btn   = document.querySelector('#modal-btn')
+var modal = document.querySelector('.modal')
+var close = document.querySelector('.close')
+
+btn.addEventListener('click', function(){
+    modal.classList.remove('hidden')
+})
+
+close.addEventListener('click', function(){
+    modal.classList.add('hidden')
+})
+
+// submit on enter for form
 $("#say-input").keypress(function(event) {
     if (event.which == 13) {
         event.preventDefault();
