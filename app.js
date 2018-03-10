7d150c3a1e7279d8acc18da4f74827de0c4ed72b
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2018-03-10 18:12:31 -0500

    MVP complete

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 const app         = express();
 const PORT        = process.env.PORT || 3000;
 const auth        = require('./auth.json');
+var db;
 
-var db
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 
-
-app.get('/', function(request, response){
+app.get('/', function (req, res) {
 
   var cursor = db.collection('says').find().toArray(function (err, results) {
-      response.render('pages/index.ejs', {
-        says: results.reverse()
-      })
+    res.render('pages/index.ejs', {
+      says: results.reverse()
+    })
   })
 })
 
-app.post('/say', function(request, response){
-  console.log(request.body)
-  db.collection('says').save(request.body, function(err, result){
-    if (err) return console.log(err)
-    console.log(request.body)
-    response.redirect('/')
-  })
+app.post('/say', function (req, res) {
+  if (req.body.say === "" || req.body.say.length >   198) {
+    res.redirect('/')
+  } else {
+    db.collection('says').save(req.body, function (err, result) {
+      if (err) return console.log(err)
+      res.redirect('/')
+    })
+  }
 })
 
-MongoClient.connect(auth.MONGO_KEY, function(err, client){
+MongoClient.connect(auth.MONGO_KEY, function (err, client) {
   if (err) return console.log(err)
   db = client.db('sayit-dev') // whatever your database name is
-  app.listen(PORT, function(){
+  app.listen(PORT, function () {
     console.log('listening on ' + PORT)
   })
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 var btn   = document.querySelector('#modal-btn')
 var modal = document.querySelector('.modal')
 var close = document.querySelector('.close')
+var mark  = document.querySelector('.question')
+var info  = document.querySelector('.info-box')
 
 btn.addEventListener('click', function(){
     modal.classList.remove('hidden')
@@ -8,6 +10,14 @@ btn.addEventListener('click', function(){
 
 close.addEventListener('click', function(){
     modal.classList.add('hidden')
+})
+
+mark.addEventListener('mouseenter', function(){
+    info.classList.remove('hidden')
+})
+
+mark.addEventListener('mouseleave', function(){
+    info.classList.add('hidden')
 })
 
 // submit on enter for form
